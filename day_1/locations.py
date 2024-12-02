3c54683c4ed5eafd004e52c78898c8350df61479
Calvin Golas <CGolas@sorenson.com>
2024-12-01 18:10:45 -0800

    Initialized project with first day skeleton

---
+++ day_1/locations.py
@@ -0,0 +1,13 @@
+# Takes in a file with two lists, returns difference between the values once they've been ordered
+
+def location_distance_diff(list_file: str) -> int:
+    # Read in both lists
+
+    # Order them as they're added to each list
+
+    # Return the difference between the two lists
+    pass
+
+
+if __name__ == '__main__':
+    location_distance_diff("locations.txt")
