dc392638a791c43fc7bf0294857ef95363ba887f
Calvin Golas <CGolas@sorenson.com>
2024-12-01 18:30:51 -0800

    Completed day 1 pt 1

--- day_1/locations.py
+++ day_1/locations.py
@@ -1,13 +1,31 @@
 # Takes in a file with two lists, returns difference between the values once they've been ordered
+import bisect
 
-def location_distance_diff(list_file: str) -> int:
+
+def find_location_distance_diff(list_file: str) -> int:
     # Read in both lists
+    file_reader = open(list_file, "r")
 
-    # Order them as they're added to each list
+    list_a = []
+    list_b = []
+
+    while file_reader.readable():
+        line = file_reader.readline()
+        if line == "":
+            break
+        split_point = line.find("   ")
+        location_a, location_b = line[:split_point], line[split_point + 3 :]
+        # Order them as they're added to each list
+        bisect.insort(list_a, int(location_a))
+        bisect.insort(list_b, int(location_b))
 
     # Return the difference between the two lists
-    pass
+    list_diff = 0
+    for i in range(0, len(list_a)):
+        list_diff += abs(list_a[i] - list_b[i])
+    return list_diff
 
 
-if __name__ == '__main__':
-    location_distance_diff("locations.txt")
+if __name__ == "__main__":
+    location_diff = find_location_distance_diff("locations.txt")
+    print(f"The difference between the two lists is {location_diff}")
